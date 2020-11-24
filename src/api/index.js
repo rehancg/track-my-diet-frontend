8f4af91737eaf305dc405853b07178aa8b906989
Rehan Chanaka <rehang@99x.lk>
2020-11-24 22:54:20 +0530

    update base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'http://143.110.186.112',
+    baseURL: 'http://143.110.186.112:1024',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });
